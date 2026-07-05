862f12a3079b54b04d0de7f85c651980cc120e5d
Kishore <kishore21092003@gmail.com>
2026-07-05 15:44:44 +0530

    deploying the project and update cradentials

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Do not use '*'
+    origin: ['https://shop-now-ibm.vercel.app', 'http://localhost:5173'],
     credentials: true
 }));
 app.use(cookieParser());
